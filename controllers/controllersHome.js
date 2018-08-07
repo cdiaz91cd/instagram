c87196803de6f73c7a5309597079ee7880543f9e
Chia <cdiaz91cd@gmail.com>
2018-08-06 22:43:43 -0300

    Changes

--- controllers/controllersHome.js
+++ controllers/controllersHome.js
@@ -1,7 +1,7 @@
 
-let self={};
+let self = {};
 
-let imagenes=[{
+let imagenes = [{
 
 	img: 'http://www.neptunepinkfloyd.co.uk/photos/var/resizes/David-Gilmour-Photos/Young-David-Gilmour-Photos/Young%20David%20Gilmour%20Photos%20%2818%29.jpg?m=1391960462',
 	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',
@@ -9,21 +9,21 @@ let imagenes=[{
 	likes: 0,
 	id: 1
 
-},{
+}, {
 	img: 'https://i.pinimg.com/originals/1b/c5/56/1bc55694d7f4fd2cfd285b585413913d.jpg',
-	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',	
+	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',
 	titulo: 'imagen 2',
 	likes: 0,
 	id: 2
-},{
+}, {
 	img: 'https://i.ytimg.com/vi/SzRDRCetXDg/maxresdefault.jpg',
-	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',	
+	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',
 	titulo: 'imagen 3',
 	likes: 0,
 	id: 3
-},{
+}, {
 	img: 'http://rs1107.pbsrc.com/albums/h381/lovedtodeth-/DAVID%20GILMOUR/480997.jpg?w=280&h=210&fit=crop',
-	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',	
+	corazon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Emoji_u2665.svg/1024px-Emoji_u2665.svg.png',
 	titulo: 'imagen 4',
 	likes: 0,
 	id: 4
@@ -31,51 +31,46 @@ let imagenes=[{
 
 
 
-self.home= function(req, res) {
-console.log(imagenes);
-		res.render('index', {//lo que yo quiero mostrar en la pantalla
+self.home = function (req, res) {
+	console.log(imagenes);
+	res.render('index', {//lo que yo quiero mostrar en la pantalla
 		fotos: imagenes
 	})
 }
 
-self.like = function(req, res) {
+self.like = function (req, res) {
 	console.log(req.body)
 
-  for (var i = 0; i < imagenes.length; i++) {
-    if (imagenes[i].id == req.body.id) {
-      imagenes[i]['likes'] = parseInt(imagenes[i]['likes']) + 1
-      res.json({likes:imagenes[i]['likes']});
+	for (var i = 0; i < imagenes.length; i++) {
+		if (imagenes[i].id == req.body.id) {
+			imagenes[i]['likes'] = parseInt(imagenes[i]['likes']) + 1
+			res.json({ likes: imagenes[i]['likes'] });
+		}
 	}
 }
-}
 
 
-self.dynamic= function(req,res){
+self.dynamic = function (req, res) {
 
 	console.log(req.params.id);
-	for(var i=0; i< imagenes.length; i++) {
-    	if (imagenes[i].id == req.params.id) {
+	for (var i = 0; i < imagenes.length; i++) {
+		if (imagenes[i].id == req.params.id) {
 			res.render('dynamic', {
-		fotos: imagenes[i]
-     	})
-  	}
-  }
+				fotos: imagenes[i]
+			})
+		}
+	}
 };
 
 
-	
+
 
 
 module.exports = self//publica porque la exportamos
 
 
 
-//1 hbs
-//1 funcion nueva en el controller
-//1 ruta
 
-
- 
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,23 @@
 
 
-var express = require('express');//es para usar un paquete1
-var router = express.Router(); //ejecuta la funcion router de express, si no hacemos eso no podemos hacer un router.get de la linea 8
-let palabras=[];
+var express = require('express');
+var router = express.Router();
+let palabras = [];
 const homeController = require('../controllers/controllersHome');
 
 
 
-router.get('/', homeController.home)//que va hacer cuando entre por esa url por get es entrar a home controller, a la home/ lo que va despues del punto es una funcion que esta dentro de homeController.
-
-
-//router.post('/', controllersHome.fotos)//estoy llamando al controlador
+router.get('/', homeController.home)
 
 router.put('/like', homeController.like)
 
-router.get('/dynamic/:id', homeController.dynamic)//puede ir cualquier nombre en dynamic
+router.get('/dynamic/:id', homeController.dynamic)
 
-router.get('/click', function(req, res, next) {
+router.get('/click', function (req, res, next) {
 
 
-	res.json({//lo que yo quiero mostrar en la pantalla
-		//frase: frasesDesordenadas[0]
+	res.json({
+
 	})
 });
 
